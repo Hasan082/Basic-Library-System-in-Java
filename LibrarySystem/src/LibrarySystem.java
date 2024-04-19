35762d3fb393a45c42b05d9db982b156446534a5
Md Hasanuzzaman <dr.has82@gmail.com>
2024-04-20 01:37:48 +0600

    Readme file updated

---
+++ LibrarySystem/src/LibrarySystem.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class LibrarySystem {
+    Map<String, Integer> books = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
